60c216ce45530c277275fa0143af7378c6ad7f32
CesarJimenezVilleda02 <A01703517@tec.mx>
2024-04-24 13:13:34 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,199 @@
+use actix_cors::Cors;
+
+use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
+
+use serde::{Deserialize, Serialize};
+
+use reqwest::Client as HttpClient;
+
+use async_trait::async_trait;
+
+use std::collections::HashMap;
+use std::fs;
+use std::io::Write;
+use std::sync::Mutex;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>,
+}
+
+impl Database {
+    fn new() -> Self {
+        Database {
+            tasks: HashMap::new(),
+            users: HashMap::new(),
+        }
+    }
+
+    // TASK RELATED FUNCTIONS
+    fn insert(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+
+    fn get(&self, id: &u64) -> Option<&Task> {
+        self.tasks.get(&id)
+    }
+
+    fn get_all(&self) -> Vec<&Task> {
+        self.tasks.values().collect()
+    }
+
+    fn delete(&mut self, id: &u64) {
+        self.tasks.remove(id);
+    }
+
+    fn update(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+
+    // USER DATA RELATED FUNCTIONS
+    fn insert_user(&mut self, user: User) {
+        self.users.insert(user.id, user);
+    }
+
+    fn get_user(&self, id: &u64) -> Option<&User> {
+        self.users.get(&id)
+    }
+
+    fn get_all_users(&self) -> Vec<&User> {
+        self.users.values().collect()
+    }
+
+    fn delete_user(&mut self, id: &u64) {
+        self.users.remove(id);
+    }
+
+    fn update_user(&mut self, user: User) {
+        self.users.insert(user.id, user);
+    }
+
+    fn get_user_by_name(&self, username: &str) -> Option<&User> {
+        self.users.values().find(|user| user.username == username)
+    }
+
+    // DATABASE SAVING
+    fn save_to_file(&self) -> std::io::Result<()> {
+        let data = serde_json::to_string(&self)?;
+        let mut file = fs::File::create("database.json")?;
+        file.write_all(data.as_bytes())?;
+        Ok(())
+    }
+
+    fn load_from_file() -> std::io::Result<Self> {
+        let file = fs::read_to_string("database.json")?;
+        let data: Database = serde_json::from_str(&file)?;
+        Ok(data)
+    }
+}
+
+struct AppState {
+    db: Mutex<Database>,
+}
+
+async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
+    let mut db = app_state.db.lock().unwrap();
+    db.insert(task.into_inner()); // extract task from web::Json
+    let _ = db.save_to_file();
+    HttpResponse::Ok().finish()
+}
+
+async fn read_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
+    let db = app_state.db.lock().unwrap();
+    match db.get(&id.into_inner()) {
+        Some(task) => HttpResponse::Ok().json(task),
+        None => HttpResponse::NotFound().finish(),
+    }
+}
+
+async fn delete_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
+    let mut db = app_state.db.lock().unwrap();
+    db.delete(&id.into_inner());
+    let _ = db.save_to_file();
+    HttpResponse::Ok().finish()
+}
+
+async fn read_all_tasks(app_state: web::Data<AppState>) -> impl Responder {
+    let db = app_state.db.lock().unwrap();
+    let tasks = db.get_all();
+    HttpResponse::Ok().json(tasks)
+}
+
+async fn update_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
+    let mut db = app_state.db.lock().unwrap();
+    db.update(task.into_inner()); // extract task from web::Json
+    let _ = db.save_to_file();
+    HttpResponse::Ok().finish()
+}
+
+async fn register(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
+    let mut db = app_state.db.lock().unwrap();
+    db.insert_user(user.into_inner()); // extract user from web::Json
+    let _ = db.save_to_file();
+    HttpResponse::Ok().finish()
+}
+
+async fn login(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
+    let db = app_state.db.lock().unwrap();
+    match db.get_user_by_name(&user.username) {
+        Some(u) => {
+            if u.password == user.password {
+                HttpResponse::Ok().body("Logged in")
+            } else {
+                HttpResponse::Unauthorized().finish()
+            }
+        }
+        None => HttpResponse::Unauthorized().finish(),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = match Database::load_from_file() {
+        Ok(data) => data,
+        Err(_) => Database::new(),
+    };
+
+    // This is a smart pointer, when cloning we clone the reference to the data
+    let data = web::Data::new(AppState { db: Mutex::new(db) });
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(
+                Cors::permissive()
+                    .allowed_origin_fn(|origin, _req_head| {
+                        origin.as_bytes().starts_with(b"http://localhost") || origin == "null"
+                    })
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .max_age(3600),
+            )
+            .app_data(data.clone())
+            .route("/task", web::post().to(create_task))
+            .route("/task", web::get().to(read_all_tasks))
+            .route("/task/{id}", web::get().to(read_task))
+            .route("/task/{id}", web::delete().to(delete_task))
+            .route("/task", web::put().to(update_task))
+            .route("/register", web::post().to(register))
+            .route("/login", web::post().to(login))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
